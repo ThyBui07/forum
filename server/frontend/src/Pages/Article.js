b01930a735e6b61e0d19ee5e6996a64e242af989
ThyBui07 <buithinguyenthy@gmail.com>
2023-02-06 21:45:58 +0200

    fix to easy reading

--- server/frontend/src/Pages/Article.js
+++ server/frontend/src/Pages/Article.js
@@ -11,7 +11,6 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import { PlusLg } from "react-bootstrap-icons";
 
 const image = {
   width: '5%',
@@ -22,28 +21,27 @@ const image = {
 class Article extends Component {
     render() {
         return (
-            <Row>
+        <Row>
           <Col lg={2} md={1} className="d-none d-lg-block d-md-block"></Col>
           
           <Col lg={8} md={10} xs={12}>
             <TopNav />
-            
-<article >
-<h2 className="mb-1">New feature</h2>
-<p className="">December 14, 2020 by <a href="#">Chris</a></p>
+            <article >
+              <h2 className="mb-1">New feature</h2>
+              <p className="">December 14, 2020 by <a href="#">Chris</a></p>
 
-<p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-<ul>
-<li>First list item</li>
-<li>Second list item with a longer description</li>
-<li>Third list item to close it out</li>
-</ul>
-<p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
-<Votes />
-</article>
-<hr />
-<Card className='mb-3'>
-<Card.Body>
+              <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
+              <ul>
+              <li>First list item</li>
+              <li>Second list item with a longer description</li>
+              <li>Third list item to close it out</li>
+              </ul>
+              <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
+              <Votes />
+            </article>
+            <hr />
+            <Card className='mb-3'>
+              <Card.Body>
                 <div className='d-flex mb-3'>
                 <Card.Img style={image} src="http://bootdey.com/img/Content/user_1.jpg"  alt="user profile image" className='me-2' />
                 <div class="input-group">
@@ -53,11 +51,11 @@ class Article extends Component {
                 <div className='d-flex flex-row-reverse'>
                   <Button variant="outline-dark">Comment</Button>
                 </div>
-                </Card.Body>
-</Card>
-<Comment />
-<Comment />
-<Comment />
+              </Card.Body>
+            </Card>
+            <Comment />
+            <Comment />
+            <Comment />
 
 
           </Col>
