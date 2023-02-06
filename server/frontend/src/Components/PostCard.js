855ad56c4bc7e3302316cb0df1827c27b3d08242
ThyBui07 <buithinguyenthy@gmail.com>
2023-02-06 22:38:30 +0200

    account page

---
+++ server/frontend/src/Components/PostCard.js
@@ -0,0 +1,43 @@
+import { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import { HandThumbsUp, HandThumbsDown, Chat } from 'react-bootstrap-icons'
+
+
+class PostCard extends Component {
+    render() {
+        return (
+            <Col key={this.props.id}>
+            <Card >
+              <Card.Body>
+                <Card.Title>{this.props.title}</Card.Title>
+                <small className='text-muted'>Posted by: {this.props.author}</small>
+                <Card.Text>
+                  {this.props.content}
+                </Card.Text>
+                <div className='d-inline-flex'>
+                  <Card.Text className='me-3'>
+                    <HandThumbsUp /> 10 Likes
+                  </Card.Text>
+                  <Card.Text className='me-3'>
+                    <HandThumbsDown  /> 10 Dislikes
+                  </Card.Text>
+                  <Card.Text className='me-3'>
+                    <Chat /> 10 Comments
+                  </Card.Text>
+                </div>
+              </Card.Body>
+              <Card.Footer>
+                <small className='text-muted'>
+                  {this.props.categories}
+                </small>
+              </Card.Footer>
+            </Card>
+          </Col>  
+        )
+    }
+}
+
+
+export default PostCard
--- server/frontend/src/Pages/MyAccountPage.js
+++ server/frontend/src/Pages/MyAccountPage.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import TopNav from '../Components/TopNav'
-import SettingsNav from '../Components/SettingsNav';
 import AccountSettings from '../Components/MyAccountSettings';
+
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+import PostCard from "../Components/PostCard";
 
 
 //import { useState, useEffect } from "react";
@@ -12,13 +17,59 @@ class MyAccountPage extends Component {
   }
   render() {
     return (
-          <div>
-            <TopNav />
-            <SettingsNav />
-            <AccountSettings />
-          </div>
-      );
-    }
+<Row>
+  <Col lg={2} md={1} className="d-none d-lg-block d-md-block"></Col>
+
+  <Col lg={8} md={10} xs={12}>
+    <TopNav />
+    <Tab.Container id="left-tabs-example" defaultActiveKey="myAccount">
+      <Row>
+        <Col sm={3}>
+        <Nav variant="pills"  className="flex-column">
+          <Nav.Item >
+            <Nav.Link className="bg-light text-dark" eventKey="myAccount">Account</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="bg-light text-dark" eventKey="myPosts">My Posts</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="bg-light text-dark" eventKey="myLikedPosts">My Liked Posts</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="bg-light text-dark" eventKey="MyCommentedPosts">My Commented Posts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        </Col>
+        <Col sm={9}>
+        <Tab.Content>
+          <Tab.Pane eventKey="myAccount">
+            <p>myAccount content</p>
+          </Tab.Pane>
+          <Tab.Pane eventKey="myPosts">
+            <p>myPosts content</p>
+            {/* items range later here */}
+            <PostCard id="1" title="hello" content="world" author="gin" categories="Sweet, Savoury" />
+          </Tab.Pane>
+          <Tab.Pane eventKey="myLikedPosts">
+            <p>myLikedPosts content</p>
+            {/* items range later here */}
+            <PostCard id="1" title="hello" content="world" author="gin" categories="Sweet, Savoury" />
+          </Tab.Pane>
+          <Tab.Pane eventKey="MyCommentedPosts">
+            <p>MyCommentedPosts content</p>
+            {/* items range later here */}
+            <PostCard id="1" title="hello" content="world" author="gin" categories="Sweet, Savoury" />
+          </Tab.Pane>
+        </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container> 
+  </Col>
+
+  <Col lg={2} md={1} className="d-none d-lg-block d-md-block"></Col>
+</Row>
+    );
+  }
 }
 
 export default MyAccountPage;
