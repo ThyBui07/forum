fe5a6a7fcd485791e259b8e0ced9cf03e0fcb126
ThyBui07 <buithinguyenthy@gmail.com>
2023-02-07 19:42:52 +0200

    create route with params, remove not use import

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,19 +4,17 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home'
 import MyAccountPage from './Pages/MyAccountPage'
-/* import Signin from './Pages/Sigin-copy' */
 import CreatePost from './Pages/CreatePost'
 import MyLikes from './Components/MyLikes';
 import LogIn from './Pages/LogIn'
 import Signup from './Pages/Signup'
 import Article from './Pages/Article';
-//import MyPosts from './Pages/MyPosts';
-//import SinglePost from './Pages/SinglePost';
 
 const categories = ['Apetizer', 'Beverage', 'Breakfast', 'Comfort food', 'Lunch', 'Salad', 'Smothie', 'Snack', 'Soup', 'Vegan', 'Savoury', 'Sweet'];
 
 
 class App extends Component {
+  
   render () {
     return (
       <Router>
@@ -25,8 +23,8 @@ class App extends Component {
           <Route exact path='/' element={<Home categories={categories}/>} />
           <Route exact path='/account' element={<MyAccountPage/>} />
           <Route exact path='/login' element={<LogIn/>} />
-          <Route exact path='/create-post' element={<CreatePost categories={categories}/>} />
-          <Route exact path='/article' element={<Article/>} />
+          <Route exact path='/create-post' element={<CreatePost/>} />
+          <Route exact path='/articles/:id' element={<Article />} />
           
         </Routes>
       </Router>
